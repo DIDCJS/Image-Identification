960316a0a06ee627b4e9137fbafe2c38d7a1fc40
psychopass <453642364@qq.com>
2017-12-27 22:45:15 +0800

    布局页与主页修改

---
+++ Image-Identification/Models/Result.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Image_Identification.Models
+{
+    public class Result
+    {
+        public UInt64 log_id { get; set; }
+
+            
+    }
+}
